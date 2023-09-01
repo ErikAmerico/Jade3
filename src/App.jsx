36414b8570f59d9ba2892d23e25a15a08608ded8
Erik Olson <erikamerico1991@gmail.com>
2023-09-01 01:12:44 -0400

    Home page renders with Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-//import { useState } from 'react'
 
-import './App.css'
+// import { ParallaxProvider } from 'react-scroll-parallax';
+// import { Parallax } from 'react-scroll-parallax';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// // import MenuItems from './MenuItems'; // Import your menu items component
+// // import ContactForm from './ContactForm'; // Import your contact form component
+// import './App.css';
+
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ParallaxProvider } from 'react-scroll-parallax';
+// import { Parallax } from 'react-scroll-parallax';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import MenuItems from './MenuItems'; // Import your menu items component
+// import ContactForm from './ContactForm'; // Import your contact form component
+import Home from './components/Home';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <ParallaxProvider>
+      <div className="App">
+        <Home />
+      </div>
+    </ParallaxProvider>
+  );
 }
 
-export default App
+export default App;
