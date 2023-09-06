3f4c8b0994e060611d9d95ca358fb19c0021b19b
Erik Olson <erikamerico1991@gmail.com>
2023-09-06 00:55:39 -0400

    started figuring out parallax background imagery

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,32 +6,25 @@ export default function Home() {
     return (
       <div>
         <Parallax className="parallax-section" y={[-20, 20]} tagouter="figure">
-          <div className="parallax-content">
+          <div className="parallax-content container-fluid">
             <h1>Welcome to Our Restaurant</h1>
             <p>Delicious Food &amp; Great Ambience</p>
           </div>
         </Parallax>
+
+        <div className="welcomeparallax">
+        </div>
   
         <Parallax className="parallax-section" y={[40, -40]} tagouter="figure">
-          <div className="parallax-content">
-            <Container>
-              <Row>
-                <Col md={6}>
-                  <h2>About Us</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...</p>
-                </Col>
-                <Col md={6}>
+          <div className="parallax-content container-fluid">
                   <h2>Our Menu</h2>
                   {/* <MenuItems /> */}
                   <p>the menu</p>
-                </Col>
-              </Row>
-            </Container>
           </div>
         </Parallax>
   
         <Parallax className="parallax-section" y={[20, -20]} tagouter="figure">
-          <div className="parallax-content">
+          <div className="parallax-content container-fluid">
             <h2>Contact Us</h2>
             {/* <ContactForm /> */}
             <p>contact info</p>
@@ -39,7 +32,7 @@ export default function Home() {
         </Parallax>
 
         <Parallax className="parallax-section" y={[20, -20]} tagouter="figure">
-          <div className="parallax-content">
+          <div className="parallax-content container-fluid">
           <div className="elfsight-app-72653ebf-0af8-4da1-bc2e-f0bbdb08275a" id='reviews'></div>
           </div>
         </Parallax>
