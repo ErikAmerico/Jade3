3dd337fde6dbb7cbea61f2b49038996a12a3952f
Erik Olson <erikamerico1991@gmail.com>
2023-09-07 13:58:51 -0400

    menu buttons are presentable on laptop

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Parallax } from 'react-scroll-parallax';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
@@ -17,14 +17,27 @@ export default function Menu() {
     setCurrentSlide((prevSlide) => (prevSlide + 1) % 2);
   };
 
-  // const goPrev = () => {
-  //   setCurrentSlide((prevSlide) => (prevSlide - 1 + 2) % 2);
-  // };
+  useEffect(() => {
+    const prevPageContain = document.getElementById('menuPrevPageContain');
+    const nextPageContain = document.getElementById('menuNextPageContain');
 
+    if (currentSlide === 0) {
+      if (prevPageContain) prevPageContain.style.display = 'none';
+      if (nextPageContain) nextPageContain.style.display = 'block';
+    } else {
+      if (prevPageContain) prevPageContain.style.display = 'block';
+      if (nextPageContain) nextPageContain.style.display = 'none';
+    }
+  }, [currentSlide]);
 
   return (
     <div>
       <Parallax className="parallax-section" id='menuParallaxSection' y={[-20, 20]} tagouter="figure">
+
+      <div className="carousel-navigation" id='menuPrevPageContain'>
+        <Button onClick={goNext} id="menuPrevPage" className='menu-button'>&#9668;&#9668;</Button>
+      </div>
+
         <Container fluid className="menu-container mt-5 mb-5">
           <Row className="justify-content-center">
             <Col md={11}>
@@ -47,9 +60,8 @@ export default function Menu() {
           </Row>
         </Container>
 
-        <div className="carousel-navigation">
-        {/* <Button onClick={goPrev} className="carousel-button">Previous</Button> */}
-        <Button onClick={goNext} className="carousel-button">Turn Page</Button>
+        <div className="carousel-navigation" id='menuNextPageContain'>
+        <Button onClick={goNext} id="menuNextPage" className='menu-button'>&#9658;&#9658;</Button>
       </div>
 
       </Parallax>
